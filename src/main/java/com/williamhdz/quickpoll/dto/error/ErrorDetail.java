9edb27f7b92f6ad457cdee2ffc062685777f6ebd
Willian <williamhernandez450@gmail.com>
2023-04-26 16:46:48 -0600

    Modify RestExceptionHandler

--- src/main/java/com/williamhdz/quickpoll/dto/error/ErrorDetail.java
+++ src/main/java/com/williamhdz/quickpoll/dto/error/ErrorDetail.java
@@ -1,5 +1,9 @@
 package com.williamhdz.quickpoll.dto.error;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ErrorDetail {
 	
 	private String title;
@@ -8,35 +12,55 @@ public class ErrorDetail {
 	private long timeStamp;
 	private String developerMessage;
 	
+	private Map<String, List<ValidationError>> errors = new HashMap<String,
+			List<ValidationError>>();
+	
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public String getDetail() {
 		return detail;
 	}
+
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
+
 	public long getTimeStamp() {
 		return timeStamp;
 	}
+
 	public void setTimeStamp(long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
+
 	public String getDeveloperMessage() {
 		return developerMessage;
 	}
+
 	public void setDeveloperMessage(String developerMessage) {
 		this.developerMessage = developerMessage;
+	}
+
+	public Map<String, List<ValidationError>> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(Map<String, List<ValidationError>> errors) {
+		this.errors = errors;
 	}
 	
 }
--- src/main/java/com/williamhdz/quickpoll/handler/RestExceptionHandler.java
+++ src/main/java/com/williamhdz/quickpoll/handler/RestExceptionHandler.java
@@ -1,30 +1,59 @@
 package com.williamhdz.quickpoll.handler;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.williamhdz.quickpoll.dto.error.ErrorDetail;
-import com.williamhdz.quickpoll.exception.ResourceNotFoundException;
+import com.williamhdz.quickpoll.dto.error.ValidationError;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class RestExceptionHandler {
 
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
-		ErrorDetail errorDetail = new ErrorDetail();
-		errorDetail.setTimeStamp(new Date().getTime());
-		errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
-		errorDetail.setTitle("Resource Not Found");
-		errorDetail.setDetail(rnfe.getMessage());
-		errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
 		
-		return new ResponseEntity<> (errorDetail, null, HttpStatus.NOT_FOUND);
+		ErrorDetail errorDetail = new ErrorDetail();
+		// Populate errorDetail instance
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+		
+		String requestPath = (String) request.getAttribute("jakarta.servlet.error.request_uri");
+		if (requestPath == null) {
+			requestPath = request.getRequestURI();
+		}
+		errorDetail.setTitle("Validation Failed");
+		errorDetail.setDetail("Input validation failed");
+		errorDetail.setDeveloperMessage(manve.getClass().getName());
+		
+		// Create ValidationError instances
+		List<FieldError> fieldErrors = manve.getBindingResult().getFieldErrors();
+		for (FieldError fieldError : fieldErrors) {
+			
+			List<ValidationError> validationErrorList = errorDetail.getErrors().get(fieldError.getField());
+			if(validationErrorList == null) {
+				validationErrorList = new
+				ArrayList<ValidationError>();
+				errorDetail.getErrors().put(fieldError.getField(), validationErrorList);
+			}
+			
+			ValidationError validationError = new ValidationError();
+			validationError.setCode(fieldError.getCode());
+			validationError.setMessage(fieldError.getDefaultMessage());
+			validationErrorList.add(validationError);
+		}
+		
+		return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+		
 	}
 	
 }
