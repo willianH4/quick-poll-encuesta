08fe1731ab5f96f7786d795f36691aa012d4e136
Willian <williamhernandez450@gmail.com>
2023-04-27 06:36:37 -0600

    modify response badrequest

--- src/main/java/com/williamhdz/quickpoll/handler/RestExceptionHandler.java
+++ src/main/java/com/williamhdz/quickpoll/handler/RestExceptionHandler.java
@@ -5,40 +5,52 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.williamhdz.quickpoll.dto.error.ErrorDetail;
 import com.williamhdz.quickpoll.dto.error.ValidationError;
+import com.williamhdz.quickpoll.exception.ResourceNotFoundException;
 
 import jakarta.inject.Inject;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@Inject
 	private MessageSource messageSource;
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public @ResponseBody ErrorDetail handleValidationError(MethodArgumentNotValidException manve, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException rnfe, HttpServletRequest request) {
+		
+		ErrorDetail errorDetail = new ErrorDetail();
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+		errorDetail.setTitle("Resource Not Found");
+		errorDetail.setDetail(rnfe.getMessage());
+		errorDetail.setDeveloperMessage(rnfe.getClass().getName());
+		
+		return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+		
+	}
+	
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException manve, HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
 		ErrorDetail errorDetail = new ErrorDetail();
 		// Populate errorDetail instance
 		errorDetail.setTimeStamp(new Date().getTime());
 		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
-		
-		String requestPath = (String) request.getAttribute("jakarta.servlet.error.request_uri");
-		if (requestPath == null) {
-			requestPath = request.getRequestURI();
-		}
 		errorDetail.setTitle("Validation Failed");
 		errorDetail.setDetail("Input validation failed");
 		errorDetail.setDeveloperMessage(manve.getClass().getName());
@@ -52,15 +64,25 @@ public class RestExceptionHandler {
 				validationErrorList = new ArrayList<ValidationError>();
 				errorDetail.getErrors().put(fieldError.getField(), validationErrorList);
 			}
-			
 			ValidationError validationError = new ValidationError();
 			validationError.setCode(fieldError.getCode());
 			validationError.setMessage(messageSource.getMessage(fieldError, null));
 			validationErrorList.add(validationError);
+			
 		}
 		
-		return errorDetail;
+		return handleExceptionInternal(manve, errorDetail, headers, status, request);
+	}
+	
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		ErrorDetail errorDetail = new ErrorDetail();
+		errorDetail.setTimeStamp(new Date().getTime());
+		errorDetail.setStatus(status.value());
+		errorDetail.setTitle("Message Not Readable");
+		errorDetail.setDetail(ex.getMessage());
+		errorDetail.setDeveloperMessage(ex.getClass().getName());
 		
+		return handleExceptionInternal(ex, errorDetail, headers, status, request);
 	}
 	
 }
