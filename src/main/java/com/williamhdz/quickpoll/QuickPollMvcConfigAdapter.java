072ade3be78bb5ba30f322f39f6a12251d4c9e7f
Willian <williamhernandez450@gmail.com>
2023-04-29 07:37:19 -0600

    Add class config for 5 ister by default getAll polls

---
+++ src/main/java/com/williamhdz/quickpoll/QuickPollMvcConfigAdapter.java
@@ -0,0 +1,20 @@
+package com.williamhdz.quickpoll;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+@Configuration
+public class QuickPollMvcConfigAdapter {
+
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		PageableHandlerMethodArgumentResolver phmar = new PageableHandlerMethodArgumentResolver();
+		// Set the default size to 5
+		phmar.setFallbackPageable(PageRequest.of(0, 5));
+		argumentResolvers.add(phmar);
+	}
+	
+}
